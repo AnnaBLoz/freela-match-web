bca860da6f9e9a649ae075928b41c81c4435eb6a
AnnaBLoz <anninha_beatriz@terra.com.br>
2025-12-07 00:29:41 -0300

    ajustes deepsource

--- src/app/company/profile/profile-info/profile-info.component.ts
+++ src/app/company/profile/profile-info/profile-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { GeneralService } from 'src/app/core/services/generalService.service';
 import { Skill } from 'src/app/core/services/profileService.service';
 
